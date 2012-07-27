80c83e77735bb15ebcef8a30e729a44bcc70a4f7
Satrajit Ghosh <satra@mit.edu>
2012-07-27 14:46:10 -0400

    sty: cleaned up script formatting

--- groupdifference.py
+++ groupdifference.py
@@ -21,9 +21,19 @@ estimator = mem.cache(spm.EstimateModel)
 cestimator = mem.cache(spm.EstimateContrast)
 
 ldesres =  designer(in_files = lgroup)
-lestres = estimator(spm_mat_file=ldesres.outputs.spm_mat_file, estimation_method={'Classical':None})
-lcestres = cestimator(spm_mat_file=lestres.outputs.spm_mat_file, beta_images=lestres.outputs.beta_images, residual_image=lestres.outputs.residual_image, group_contrast=True, contrasts=[('LGroup', 'T', ['mean'], [1])])
+lestres = estimator(spm_mat_file=ldesres.outputs.spm_mat_file,
+                    estimation_method={'Classical':None})
+lcestres = cestimator(spm_mat_file=lestres.outputs.spm_mat_file,
+                      beta_images=lestres.outputs.beta_images,
+                      residual_image=lestres.outputs.residual_image,
+                      group_contrast=True,
+                      contrasts=[('LGroup', 'T', ['mean'], [1])])
 
 hdesres =  designer(in_files = hgroup)
-hestres = estimator(spm_mat_file=hdesres.outputs.spm_mat_file, estimation_method={'Classical':None})
-hcestres = cestimator(spm_mat_file=hestres.outputs.spm_mat_file, beta_images=hestres.outputs.beta_images, residual_image=hestres.outputs.residual_image, group_contrast=True, contrasts=[('LGroup', 'T', ['mean'], [1])])
+hestres = estimator(spm_mat_file=hdesres.outputs.spm_mat_file,
+                    estimation_method={'Classical':None})
+hcestres = cestimator(spm_mat_file=hestres.outputs.spm_mat_file,
+                      beta_images=hestres.outputs.beta_images,
+                      residual_image=hestres.outputs.residual_image,
+                      group_contrast=True,
+                      contrasts=[('LGroup', 'T', ['mean'], [1])])
